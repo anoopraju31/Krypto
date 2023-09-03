6e3e3e2f1abe65a1cbbea02bd8a0558f75cfcb65
anoopraju31 <anoopraju31@gmail.com>
2023-09-03 22:37:52 +0530

    Footer Component- initial

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,3 +1,4 @@
 export { default as SideNavbar } from './SideNavbar'
 export { default as CurrencyCard } from './CurrencyCard'
 export { default as NewsCard } from './NewsCard'
+export { default as Footer } from './Footer'
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer'
 import SideNavbar from './components/SideNavbar'
 import './globals.css'
 import type { Metadata } from 'next'
@@ -19,6 +20,7 @@ export default function RootLayout({
 					<SideNavbar />
 					{children}
 				</div>
+				<Footer />
 			</body>
 		</html>
 	)
