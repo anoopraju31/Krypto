822a0dca70472e586ec52935d18ab267914a887e
anoopraju31 <anoopraju31@gmail.com>
2023-09-03 13:06:07 +0530

    Side Navbar Component: initial

---
+++ src/app/components/SideNavbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SideNavbar = () => {
+	return <div>SideNavbar</div>
+}
+
+export default SideNavbar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import SideNavbar from './components/SideNavbar'
 import './globals.css'
 import type { Metadata } from 'next'
 
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body>{children}</body>
+			<body>
+				<SideNavbar />
+				{children}
+			</body>
 		</html>
 	)
 }
